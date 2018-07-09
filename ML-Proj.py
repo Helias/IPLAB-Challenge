59e3553f1460fa7edff910eef4a42422d9024c1d
Helias <stefanoborzi32@gmail.com>
2018-07-09 15:44:15 +0200

    Updated project, removed useless comments

--- ML-Proj.py
+++ ML-Proj.py
@@ -40,9 +40,7 @@ def plot_logs_classification(logs):
 class MiniAlexNetV3(nn.Module):
     def __init__(self, input_channels=3, out_classes=16):
         super(MiniAlexNetV3, self).__init__()
-        #ridefiniamo il modello utilizzando i moduli sequential.
-        #ne definiamo due: un "feature extractor", che estrae le feature maps
-        #e un "classificatore" che implementa i livelly FC
+
         self.feature_extractor = nn.Sequential(
             #Conv1
             nn.Conv2d(input_channels, 16, 5, padding=2), #Input: 3 x 28 x 28. Ouput: 16 x 28 x 28
@@ -69,7 +67,7 @@ class MiniAlexNetV3(nn.Module):
         )
 
         self.classifier = nn.Sequential(
-            nn.Dropout(), #i layer di dropout vanno posizionati prima di FC6 e FC7
+            nn.Dropout(),
             #FC6
             nn.Linear(2304, 2048), #Input: 256 * 3 * 3
             nn.ReLU(),
@@ -85,52 +83,32 @@ class MiniAlexNetV3(nn.Module):
 
 
     def forward(self,x):
-        #Applichiamo le diverse trasformazioni in cascata
         x = self.feature_extractor(x)
         x = self.classifier(x.view(x.shape[0],-1))
         return x
 
-class ScenesDataset2(Dataset):
-    """Implementa l'oggetto ScenesDataset che ci permette di caricare
-    le immagini del dataset 8 Scenes"""
+class LocalDataset(Dataset):
     def __init__(self,base_path,txt_list,transform=None):
-        """Input:
-            base_path: il path alla cartella contenente le immagini
-            txt_list: il path al file di testo contenente la lista delle immagini
-                        con le relative etichette. Ad esempio train.txt o test.txt.
-            transform: implementeremo il dataset in modo che esso supporti le trasformazioni"""
-        #conserviamo il path alla cartella contenente le immagini
         self.base_path=base_path
-        #carichiamo la lista dei file
-        #sarà una matrice con n righe (numero di immagini) e 2 colonne (path, etichetta)
         self.data = np.loadtxt(txt_list,dtype=str,delimiter=',')
-        #conserviamo il riferimento alla trasformazione da applicare
         self.transform = transform
 
     def __getitem__(self, index):
-        #recuperiamo il path dell'immagine di indice index e la relativa etichetta
-        f, x, y, u, v, c = self.data[index]
+        img_path, x, y, u, v, c = self.data[index]
 
-        #carichiamo l'immagine utilizzando PIL
-        im = Image.open(path.join(self.base_path, f))
+        im = Image.open(path.join(self.base_path, img_path))
 
-        #se la trasfromazione è definita, applichiamola all'immagine
         if self.transform is not None:
             im = self.transform(im)
 
-        #convertiamo l'etichetta in un intero
         label = int(c)
 
-        #restituiamo un dizionario contenente immagine etichetta
         return {'image' : im, 'label': label}
 
-    #restituisce il numero di campioni: la lunghezza della lista "images"
     def __len__(self):
         return len(self.data)
 
 def train_classification(model, lr=0.01, epochs=20, momentum=0.9, weight_decay = 0.000001, train_loader=0, test_loader=0):
-
-
     criterion = nn.CrossEntropyLoss()
     optimizer = SGD(model.parameters(),lr, momentum=momentum, weight_decay=weight_decay)
 
@@ -138,12 +116,13 @@ def train_classification(model, lr=0.01, epochs=20, momentum=0.9, weight_decay =
     losses = {'train':[], 'test':[]}
     accuracies = {'train':[], 'test':[]}
     preds = {'train':[], 'test':[]}
+
     if torch.cuda.is_available():
-        model=model.cuda()
+        model = model.cuda()
 
     for e in range(epochs):
         for mode in ['train', 'test']:
-            if mode=='train':
+            if mode == 'train':
                 model.train()
             else:
                 model.eval()
@@ -152,7 +131,7 @@ def train_classification(model, lr=0.01, epochs=20, momentum=0.9, weight_decay =
             epoch_acc = 0
             samples = 0
             for i, batch in enumerate(loaders[mode]):
-                #trasformiamo i tensori in variabili
+                # tensor -> variables
                 x=Variable(batch['image'], requires_grad=True)
                 y=Variable(batch['label'])
 
@@ -197,33 +176,32 @@ def train_classification(model, lr=0.01, epochs=20, momentum=0.9, weight_decay =
     return model, (losses, accuracies)
 
 
-train_set2 = ScenesDataset2('prj_dataset/images','prj_dataset/training_list.txt',transform=transforms.ToTensor())
-test_set2 = ScenesDataset2('prj_dataset','prj_dataset/validation_list.txt',transform=transforms.ToTensor())
+train_set2 = LocalDataset('prj_dataset/images','prj_dataset/training_list.txt',transform=transforms.ToTensor())
+test_set2 = LocalDataset('prj_dataset','prj_dataset/validation_list.txt',transform=transforms.ToTensor())
 
-m2 = np.zeros(3)
+# mean, dev st. -> normalization
+
+# mean
+m = np.zeros(3)
 for sample in train_set2:
-    m2 += sample['image'].sum(1).sum(1) #accumuliamo la somma dei pixel canale per canale
+    m += sample['image'].sum(1).sum(1) # sum pixels per channels
+m = m/(len(train_set2)*256*256) # num_img * num_pixels
 
-#dividiamo per il numero di immagini moltiplicato per il numero di pixel
-m2 = m2/(len(train_set2)*256*256)
-
-#procedura simile per calcolare la deviazione standard
-s2 = np.zeros(3)
+# dev st.
+s = np.zeros(3)
 for sample in train_set2:
-    s2 += ((sample['image']-torch.Tensor(m2).view(3,1,1))**2).sum(1).sum(1)
-
-s2 = np.sqrt(s2/(len(train_set2)*256*256))
-
+    s += ((sample['image']-torch.Tensor(m).view(3,1,1))**2).sum(1).sum(1)
+s = np.sqrt(s/(len(train_set2)*256*256))
 
 transform_prj = transforms.Compose([transforms.RandomVerticalFlip(),
                                     transforms.ColorJitter(),
                                     transforms.RandomResizedCrop(28),
                                     transforms.ToTensor(),
-                                    transforms.Normalize(m2,s2)
+                                    transforms.Normalize(m,s)
                                    ])
 
-train_set = ScenesDataset2('prj_dataset/images','prj_dataset/training_list.txt',transform=transform_prj)
-test_set = ScenesDataset2('prj_dataset/images','prj_dataset/validation_list.txt',transform=transform_prj)
+train_set = LocalDataset('prj_dataset/images','prj_dataset/training_list.txt',transform=transform_prj)
+test_set = LocalDataset('prj_dataset/images','prj_dataset/validation_list.txt',transform=transform_prj)
 
 train_loader = DataLoader(train_set, batch_size=32, num_workers=2, shuffle=True)
 test_loader = DataLoader(test_set, batch_size=32, num_workers=2)
