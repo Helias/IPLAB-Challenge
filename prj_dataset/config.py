28b3b0b85d5b59a4ed17873b9914bde8413be41a
Helias <stefanoborzi32@gmail.com>
2019-01-14 19:14:09 +0100

    Imported the new project

---
+++ prj_dataset/config.py
@@ -0,0 +1,21 @@
+# Configuration
+
+DEBUG = True
+
+# training
+EPOCHS = 1
+MOMENTUM = 0.9
+LEARNING_RATE = 0.01
+BATCH_SIZE = 32
+THREADS = 8
+
+# file paths
+IMAGES_PATH = "images"
+TRAINING_PATH = "training_list.csv"
+VALIDATION_PATH = "validation_list.csv"
+TEST_PATH = "validation_list.csv"
+
+RESULTS_PATH="results"
+
+# regularization
+WEIGHT_DECAY=0.005
